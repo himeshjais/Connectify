58602aeffb21cf0e67bec08ad99cfbfa0a2c3f30
himeshjais <himeshjaiswal.hj@gmail.com>
2026-04-02 18:37:59 +0530

    fix: image path case issue

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -120,7 +120,7 @@ const LoginPage = () => {
           <div className="max-w-md p-8">
             {/* Illustration */}
             <div className="relative aspect-square max-w-sm mx-auto">
-              <img src="/VideoCall.png" alt="Language connection illustration" className="w-full h-full" />
+              <img src="/Videocall.png" alt="Language connection illustration" className="w-full h-full" />
             </div>
 
             <div className="text-center space-y-3 mt-6">
